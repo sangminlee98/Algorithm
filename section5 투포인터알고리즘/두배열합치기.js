25abaf0a76bae7fdb9336d91487a31ca128c5ffc
sangminlee98 <tkdals0975@naver.com>
2022-05-18 22:29:05 +0900

    두배열합치기 (투포인터)

--- section5 투포인터알고리즘/두배열합치기.js
+++ section5 투포인터알고리즘/두배열합치기.js
@@ -1,8 +1,20 @@
 function solution(arr1, arr2){
   let answer=[];
-  answer.push(arr1);
-  answer.push(arr2);
-  answer.sort();
+  // answer.push(arr1);
+  // answer.push(arr2);
+  // answer.sort();
+
+  const n = arr1.length;
+  const m = arr2.length;
+  let p1=p2=0;
+
+  while(p1<n && p2<m) {
+    if(arr1[p1]<=arr2[p2]) answer.push(arr1[p1++])
+    else answer.push(arr2[p2++]);
+  };
+  while(p1<n) answer.push(arr1[p1++]);
+  while(p2<m) answer.push(arr2[p2++]);
+
   return answer;
 }
 
