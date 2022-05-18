67f45f6a7322265a9be67662a3df756954921bd1
sangminlee98 <tkdals0975@naver.com>
2022-05-18 23:25:23 +0900

    연속부분수열1 (투포인터)

--- section5 투포인터알고리즘/연속부분수열1.js
+++ section5 투포인터알고리즘/연속부분수열1.js
@@ -1,15 +1,28 @@
 function solution(m, arr){
   let answer=0;
-  for(let i=0; i<arr.length; i++) {
-    let sum = 0, lt = i;
-    while(sum<=m) {
-      sum += arr[lt];
-      if(sum == m) {
-        answer++;
-        break;
-      }
-      lt++;
-      if(lt==arr.length) break;
+  // for(let i=0; i<arr.length; i++) {
+  //   let sum = 0, lt = i;
+  //   while(sum<=m) {
+  //     sum += arr[lt];
+  //     if(sum == m) {
+  //       answer++;
+  //       break;
+  //     }
+  //     lt++;
+  //     if(lt==arr.length) break;
+  //   }
+  // }
+  let lt=0;
+  let rt=1;
+  let tmp=arr[lt]+arr[rt];
+  while(rt<arr.length) {
+    if(tmp===m) {
+      answer++;
+      tmp -= arr[lt++];
+    } else if(tmp<m) {
+      tmp+=arr[++rt];
+    } else {
+      tmp -= arr[lt++];
     }
   }
   return answer;
