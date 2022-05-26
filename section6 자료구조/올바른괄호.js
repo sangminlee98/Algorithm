dd5b1373f687f9be5e51da42a31cf3ad951c6d04
sangminlee98 <tkdals0975@naver.com>
2022-05-26 23:19:58 +0900

    올바른 괄호

---
+++ section6 자료구조/올바른괄호.js
@@ -0,0 +1,13 @@
+function solution(s){
+  let answer="YES";
+  let stack = [];
+  for(let x of s) {
+    if(x === '(') stack.push('(');
+    else stack.pop();
+  }
+  if(stack.length !== 0) return "NO";
+  return answer;
+}
+
+let a="(()()))(";
+console.log(solution(a));
