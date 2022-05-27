452e90a2be7a879fac19226523e7ab2d01cd47ab
sangminlee98 <tkdals0975@naver.com>
2022-05-27 18:09:32 +0900

    공주구하기

--- section6 자료구조/공주구하기.js
+++ section6 자료구조/공주구하기.js
@@ -1,7 +1,13 @@
 function solution(n, k){
   let answer;
   let queue=Array.from({length:n}, (v, i)=>i+1);
-  
+  let index = 0;
+  while(true) {
+    if(queue.length === 1) break;
+    index = ((index+2)%queue.length);
+    console.log(queue.splice(index%queue.length,1));
+  }
+  answer = queue[0];
   return answer;
 }
 
