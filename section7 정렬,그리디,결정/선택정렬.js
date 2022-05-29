26995345ecbe9e49a45eb3b2233fc49be5519022
sangminlee98 <tkdals0975@naver.com>
2022-05-29 22:49:06 +0900

    선택정렬

---
+++ section7 정렬,그리디,결정/선택정렬.js
@@ -0,0 +1,20 @@
+function solution(arr){
+  let answer=arr;
+  for(let i=0; i<arr.length-1; i++) {
+    let min = Number.MAX_SAFE_INTEGER;
+    let index;
+    for(let j=i+1; j<arr.length; j++) {
+      if(min>arr[j]) {
+        min = arr[j];
+        index = j;
+      }
+    };
+    let k = arr[i];
+    arr[i] = min;
+    arr[index] = k;
+  }
+  return answer;
+}
+
+let arr=[13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
