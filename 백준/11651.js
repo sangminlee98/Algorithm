1502ee6447acbfc2ba827611822d1dc478ed2545
sangminlee98 <tkdals0975@naver.com>
2022-07-27 01:14:12 +0900

    백준11651 좌표 정렬하기2

--- 백준/11651.js
+++ 백준/11651.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+input.shift();
 let position = [];
 input.forEach((item) => position.push(item.split(" ").map((item) => +item)));
 position.sort((a, b) => {
-  if (a[0] === b[0]) {
-    return a[1] - b[1];
-  } else {
+  if (a[1] === b[1]) {
     return a[0] - b[0];
+  } else {
+    return a[1] - b[1];
   }
 });
 console.log(position.join("\n").replaceAll(",", " ").trim());
