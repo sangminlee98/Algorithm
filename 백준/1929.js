11337d4e1c4c3c08548054001b6e5c935416b92d
sangminlee98 <tkdals0975@naver.com>
2022-08-08 21:17:30 +0900

    백준1929 소수 구하기(수정)

--- 백준/1929.js
+++ 백준/1929.js
@@ -3,7 +3,7 @@ let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 const start = +input[0];
 const end = +input[1];
 let answer = [];
-let arr = Array.from({ length: 1000001 }, () => true);
+let arr = Array.from({ length: end + 1 }, () => true);
 arr[0] = arr[1] = false;
 for (let i = 2; i < arr.length; i++) {
   if (!arr[i]) continue;
