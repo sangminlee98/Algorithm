ac818863bef1c6c2df09976bf02e49682998e039
sangminlee98 <tkdals0975@naver.com>
2023-03-09 22:53:40 +0900

    프로그래머스 문자열 나누기 (재귀풀이)

--- 프로그래머스/lv1/문자열 나누기.js
+++ 프로그래머스/lv1/문자열 나누기.js
@@ -1,29 +1,44 @@
-function solution(s) {
-  let answer = 0;
-  let cnt1 = 1;
-  let cnt2 = 0;
-  let char = s[0];
-  for (let x = 1; x < s.length; x++) {
-    if (char === s[x]) cnt1++;
-    else cnt2++;
-    if (cnt1 === cnt2) {
-      answer++;
-      char = s[x + 1];
-      x += 1;
-      if (x === s.length - 1) {
-        answer++;
-        break;
-      }
-      cnt1 = 1;
-      cnt2 = 0;
-    }
-    if (x === s.length - 1) {
-      answer++;
-      break;
-    }
-  }
+// function solution(s) {
+//   let answer = 0;
+//   let cnt1 = 1;
+//   let cnt2 = 0;
+//   let char = s[0];
+//   for (let x = 1; x < s.length; x++) {
+//     if (char === s[x]) cnt1++;
+//     else cnt2++;
+//     if (cnt1 === cnt2) {
+//       answer++;
+//       char = s[x + 1];
+//       x += 1;
+//       if (x === s.length - 1) {
+//         answer++;
+//         break;
+//       }
+//       cnt1 = 1;
+//       cnt2 = 0;
+//     }
+//     if (x === s.length - 1) {
+//       answer++;
+//       break;
+//     }
+//   }
 
-  return answer === 0 ? 1 : answer;
+//   return answer === 0 ? 1 : answer;
+// }
+
+// 재귀함수를 이용한 풀이
+function solution(s, count = 0) {
+  if (!s) return count;
+  const [first, ...rest] = s.split("");
+  let sameCount = 1;
+  let insameCount = 0;
+  let i = 0;
+  for (; i < rest.length; i++) {
+    if (first === rest[i]) sameCount++;
+    else insameCount++;
+    if (sameCount === insameCount) break;
+  }
+  return solution(rest.slice(i + 1).join(""), count + 1);
 }
 
 console.log(solution("abss"));
