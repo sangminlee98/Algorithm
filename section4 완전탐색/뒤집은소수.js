14722b366ca09b4b480138ce3931faefab197c63
sangminlee98 <tkdals0975@naver.com>
2023-06-04 21:44:06 +0900

    section4 뒤집은소수

--- section4 완전탐색/뒤집은소수.js
+++ section4 완전탐색/뒤집은소수.js
@@ -1,24 +1,43 @@
-const isPrime = (num) => {
-  if(num === 1) return false;
-  for(let i=2; i<parseInt(Math.sqrt(num)); i++) {
-    if(num%i === 0) return false;
-  } 
-  return true;
-}
+// const isPrime = (num) => {
+//   if(num === 1) return false;
+//   for(let i=2; i<parseInt(Math.sqrt(num)); i++) {
+//     if(num%i === 0) return false;
+//   }
+//   return true;
+// }
 
-const solution = (arr) => {
-  let answer=[];
-  for(let x of arr) {
-    let res = 0;
-    while(x>0) {
-      let t = x%10;
-      res = res*10 + t;
-      x = parseInt(x/10);
+// const solution = (arr) => {
+//   let answer=[];
+//   for(let x of arr) {
+//     let res = 0;
+//     while(x>0) {
+//       let t = x%10;
+//       res = res*10 + t;
+//       x = parseInt(x/10);
+//     }
+//     if(isPrime(res)) answer.push(res);
+//   }
+//   return answer;
+// }
+
+// let arr=[32, 55, 62, 20, 250, 370, 200, 30, 100];
+// console.log(solution(arr));
+
+function solution(nums) {
+  const answer = [];
+  let flag;
+  for (let num of nums) {
+    flag = true;
+    const reverseNum = parseInt(num.toString().split("").reverse().join(""));
+    if (!reverseNum || reverseNum === 1) flag = false;
+    for (let i = 2; i < Math.sqrt(reverseNum); i++) {
+      if (reverseNum % i === 0) flag = false;
     }
-    if(isPrime(res)) answer.push(res);
+    if (flag) answer.push(reverseNum);
+    flag = true;
   }
   return answer;
 }
 
-let arr=[32, 55, 62, 20, 250, 370, 200, 30, 100];
+let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
 console.log(solution(arr));
