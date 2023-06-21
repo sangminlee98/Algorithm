827b2994f3d525f9e31d649421aac53d257efd72
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2023-06-22 01:27:16 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,9 +1,25 @@
-let input = require("fs").readFileSync("/dev/stdin").toString().trim();
+// let input = require('fs').readFileSync('./input.txt').toString();
 
-const value = input.split("-").map((num) =>
-  num
-    .split("+")
-    .map((item) => Number(item))
-    .reduce((prev, curr) => prev + curr, 0)
-);
-console.log(eval(value.join("-")));
+// const arr = input.split('-');
+
+// const sumArr = arr.map(a => a.split("+").reduce((a, b) => Number(a) + Number(b))).map(r => Number(r));
+// let answer = sumArr[0];
+
+// for(let i =1; i<sumArr.length; i++) {
+//   answer -= sumArr[i];
+// }
+
+// console.log(answer);
+
+let input = require('fs').readFileSync('/dev/stdin').toString();
+
+let groups = input.split('-');
+let answer = 0;
+
+for(let i = 0; i < groups.length; i++) {
+  let cur = groups[i].split("+").map(Number).reduce((a,b) => a+b);
+  if(i === 0) answer += cur;
+  else answer -= cur;
+}
+
+console.log(answer);
