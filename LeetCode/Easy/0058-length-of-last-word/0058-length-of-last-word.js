2af6b8c29ebf41d7aa41cab2ad9d25bb24dcd4bb
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2023-11-06 23:36:44 +0900

    Time: 51 ms (54.1%), Space: 42.2 MB (27.23%) - LeetHub

--- LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.js
+++ LeetCode/Easy/0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,9 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-  let arr = s.split()
-  while(arr.some(item => item.includes(" "))) {
-    arr = arr.join("").trim().split(" ");
-  }
+  const arr = s.trim().split(" ");
   return arr[arr.length-1].length;
 };
