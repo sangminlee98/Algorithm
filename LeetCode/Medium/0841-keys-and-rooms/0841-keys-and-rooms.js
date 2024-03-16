60104d90e3fb53b20260833a923085bf203ca6e9
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2024-03-16 21:06:54 +0900

    Time: 61 ms (39.19%), Space: 52.5 MB (13.26%) - LeetHub

---
+++ LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {
+  const visitedRooms = Array.from({length: rooms.length}, () => false);
+  const keys = new Map()
+  rooms.map((_, idx) => keys.set(idx, false));
+  dfs(rooms, visitedRooms, 0, keys);
+  return visitedRooms.filter(visited => !visited).length === 0;
+};
+
+const dfs = (rooms, visitedRooms, roomNum, keys) => {
+  if(visitedRooms[roomNum]) return;
+  visitedRooms[roomNum] = true;
+
+  for(let i = 0; i < rooms[roomNum].length; i++) {
+    if(!keys.get(rooms[roomNum][i])) {
+      keys.set(rooms[roomNum][i], true);
+    }
+    if(keys.get(rooms[roomNum][i]) && !visitedRooms[rooms[roomNum][i]]) {
+      dfs(rooms, visitedRooms, rooms[roomNum][i], keys);
+    }
+  }
+}
