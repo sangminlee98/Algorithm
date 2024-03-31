6434acf0b881fc614c49b00cd1bf9b147bba475e
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2024-03-31 21:11:54 +0900

    Time: 56 ms (95.56%), Space: 51.5 MB (93.33%) - LeetHub

---
+++ LeetCode/Medium/0692-top-k-frequent-words/0692-top-k-frequent-words.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+  const answer = [];
+  const map = new Map();
+  for(let i = 0; i < words.length; i++) {
+    if(!map.has(words[i])) {
+      map.set(words[i], 1);
+    } else {
+      map.set(words[i], map.get(words[i])+1);
+    }
+  }
+
+  const mapArray = [...map];
+  mapArray.sort((a,b) => {
+    if(a[1] === b[1]) return b[0] < a[0] ? 1 : -1;
+    return a[1] < b[1] ? 1 : -1;
+  })
+  
+  for(let i = 0; i < k; i++) {
+    answer.push(mapArray[i][0]);
+  }
+
+  return answer;
+};
