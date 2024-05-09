7676f32484ded031c2d15fbe68129b4f69b9ede3
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2024-05-09 21:33:05 +0900

    Time: 61 ms (43.92%), Space: 51.3 MB (71.64%) - LeetHub

---
+++ LeetCode/Easy/0872-leaf-similar-trees/0872-leaf-similar-trees.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function(root1, root2) {
+  const arr1 = [];
+  const arr2 = [];
+  function dfs(root, arr) {
+    if(!root.left && !root.right) {
+      arr.push(root.val); 
+      return;
+    }
+    root.left && dfs(root.left, arr);
+    root.right && dfs(root.right, arr);
+  }
+  dfs(root1, arr1);
+  dfs(root2, arr2);
+  return arr1.join() === arr2.join() ? true : false;
+};
