8c9eda7894cefb590722239948fdcac8b9d59348
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2024-05-10 18:33:51 +0900

    Time: 128 ms (52.84%), Space: 73 MB (5.43%) - LeetHub

---
+++ LeetCode/Medium/1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+  let cnt = 0;
+  function dfs(node, max) {
+    if(!node) return;
+    if(node.val >= max) cnt++;
+    max = Math.max(node.val, max);
+    dfs(node.left, max);
+    dfs(node.right, max);
+  }
+  dfs(root, root.val);
+  return cnt;
+};
