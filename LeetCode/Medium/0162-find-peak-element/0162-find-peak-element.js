db928f1162cccee1536b9af9ef036b5f94a810dc
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2024-05-14 00:25:38 +0900

    Time: 48 ms (77.88%), Space: 49.4 MB (8.85%) - LeetHub

--- LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.js
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.js
@@ -3,30 +3,12 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-
-  if(nums.length === 1) return 0;
-
-  while(left <= right) {
-    const mid = left + right >> 1;
-
-    if(mid === 0) {
-      if(nums[mid] > nums[mid + 1]) return mid;
-      left = mid + 1;
-      continue;
-    } else if (mid === nums.length - 1) {
-      if(nums[mid] > nums[mid - 1]) return mid;
-      right = mid - 1;
-      continue;
-    }
-
-    if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-      return mid;
-    } else if(nums[mid] > nums[mid - 1]) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
+  let left = 0, right = nums.length-1, mid;
+    
+  while(left < right) {
+    mid = right + left >> 1;
+    if(nums[mid] > nums[mid+1]) right = mid;
+    else left = mid+1;
   }
+  return left;
 };
