43b81807fe5b0be5d674bf3ec66897f0e6678114
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2024-05-22 02:23:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.js
+++ LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.js
@@ -8,15 +8,14 @@ var combinationSum3 = function(k, n) {
 
   function makeCombination(combination, remainNumber) {
     if(remainNumber === 0 && combination.reduce((acc, curr) => acc + curr, 0) === n) answer.push(combination);
-    
+
     for(let i = combination[combination.length-1] ?? 0 + 1; i < (n > 9 ? 10 : n); i++) {
-      if(combination.reduce((acc, curr) => acc + curr, 0) + i <= n && combination.indexOf(i) === -1) {
+      if(combination.reduce((acc, curr) => acc + curr, 0) + i <= n) {
          makeCombination([...combination, i], remainNumber - 1);
       };
     }
   }
 
   makeCombination([], k);
-  // return [...new Set(answer.map(JSON.stringify))].map(JSON.parse);
   return answer;
 };
