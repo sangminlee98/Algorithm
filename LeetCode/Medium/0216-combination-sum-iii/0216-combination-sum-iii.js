a2a17567776e63dd6aa080b28faed55a542ccab2
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2024-05-22 02:19:42 +0900

    Time: 43 ms (95.03%), Space: 50 MB (41.36%) - LeetHub

--- LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.js
+++ LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.js
@@ -7,11 +7,9 @@ var combinationSum3 = function(k, n) {
   let answer = [];
 
   function makeCombination(combination, remainNumber) {
-    if(remainNumber === 0 && combination.reduce((acc, curr) => acc + curr, 0) === n) {
-      answer.push(combination.sort());
-      return;
-    }
-    for(let i = combination[0] ?? 0 + 1; i < (n > 9 ? 10 : n); i++) {
+    if(remainNumber === 0 && combination.reduce((acc, curr) => acc + curr, 0) === n) answer.push(combination);
+    
+    for(let i = combination[combination.length-1] ?? 0 + 1; i < (n > 9 ? 10 : n); i++) {
       if(combination.reduce((acc, curr) => acc + curr, 0) + i <= n && combination.indexOf(i) === -1) {
          makeCombination([...combination, i], remainNumber - 1);
       };
@@ -19,5 +17,6 @@ var combinationSum3 = function(k, n) {
   }
 
   makeCombination([], k);
-  return [...new Set(answer.map(JSON.stringify))].map(JSON.parse);
+  // return [...new Set(answer.map(JSON.stringify))].map(JSON.parse);
+  return answer;
 };
