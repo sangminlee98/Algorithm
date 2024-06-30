93e4eb78de3b3cdbc6e230a79a53d6e72fd337e1
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2024-06-30 23:34:56 +0900

    Time: 81 ms (37.68%), Space: 54.2 MB (51.64%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.js
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.js
@@ -3,20 +3,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let i = 0;
-    let j = 1;
-    while(j<nums.length) {
-        if(nums[i] !== 0) {
-            i++;
-            j++;
-        }
-        else {
-            if(nums[j] !== 0) {
-                [nums[i], nums[j]] = [nums[j], nums[i]];
-                i++;
-            }
-            j++;
-        }
-    }
-    return nums
+  let p = 0;
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] !== 0) {
+      [nums[p], nums[i]] = [nums[i], nums[p]]
+      p++;
+    };
+  }
 };
