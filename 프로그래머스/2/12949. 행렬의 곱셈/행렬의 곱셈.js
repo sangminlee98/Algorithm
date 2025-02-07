801c386ae68ab0a3dfeb877114fbfaaba035b05e
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2025-02-07 22:43:03 +0900

    [level 2] Title: 행렬의 곱셈, Time: 25.60 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,19 +1,21 @@
 function solution(arr1, arr2) {
     const r1 = arr1.length;
     const c1 = arr1[0].length;
-    
     const r2 = arr2.length;
     const c2 = arr2[0].length;
     
-    const result = Array.from(Array(r1), () => Array(c2).fill(0))
+    const answer = [];
+    for(let i = 0; i < r1; i++) {
+        answer.push(new Array(c2).fill(0));
+    }
     
     for(let i = 0; i < r1; i++) {
         for(let j = 0; j < c2; j++) {
             for(let k = 0; k < c1; k++) {
-                result[i][j] += arr1[i][k] * arr2[k][j];
+                answer[i][j] += arr1[i][k] * arr2[k][j]
             }
         }
     }
     
-    return result;
+    return answer;
 }
