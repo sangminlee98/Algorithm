157706ffeb036480951a01d492b52445d09cf5dc
Sangmin Lee <83197138+sangminlee98@users.noreply.github.com>
2025-03-05 00:40:54 +0900

    [level 2] Title: 구명보트, Time: 31.83 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+    let answer = 0;
+    let left = 0;
+    let right = people.length - 1;
+    people.sort((a, b) => a - b);
+    
+    while(left <= right) {
+        if(people[left] + people[right] <= limit) {
+            left++;
+            right--;
+        } else {
+            right--;
+        }
+        answer++;
+    }
+    return answer;
+}
